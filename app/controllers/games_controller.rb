dbfc461b13839f40903f5889bb0992e29ec2f0d4
Alexandre Gervais <algerli@hotmail.com>
2019-05-16 18:37:34 +0200

    Rail longest word game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,6 @@
+require 'json'
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
     charset = Array('A'..'Z')
@@ -6,6 +9,13 @@ class GamesController < ApplicationController
 
   def score
     @answer = params['answer']
-    raise
+    result = open("https://wagon-dictionary.herokuapp.com/#{@answer}").read
+    h = JSON.parse(result)
+    if h['found'] == true
+      @finalResult = 'your score is 5'
+    else
+      @finalResult = '0'
+    end
   end
 end
+
